a73ac4d17b5e06ded1cf1914ca1e34561394920f
DorinGuzun <dorin.guzun56@gmail.com>
2025-12-18 20:12:18 +0200

    Add Receiver and Sender projects

---
+++ Receiver/Services/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using GrpcAgent;
+
+namespace Receiver.Services
+{
+     public class NotificationService : Notifier.NotifierBase
+     {
+          public override Task<NotifyReply> Notify(NotifyRequest request, ServerCallContext context)
+          {
+               Console.WriteLine($"Received: {request.Content}");
+
+               return Task.FromResult( new NotifyReply() { IsSuccess = true});
+          }
+     }
+}
---
+++ Receiver/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Receiver.Services;
+
+namespace Receiver
+{
+     public class Startup
+     {
+          public void ConfigureServices(IServiceCollection services)
+          {
+               services.AddGrpc();
+          }
+
+          public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+          {
+               if (env.IsDevelopment())
+               {
+                    app.UseDeveloperExceptionPage();
+               }
+
+               app.UseRouting();
+
+               app.UseEndpoints(endpoints =>
+               {
+                    endpoints.MapGrpcService<NotificationService>();
+
+                    endpoints.MapGet("/", async context =>
+                    {
+                         await context.Response.WriteAsync("Communication with gRPC endpoints must made through a gRPC client");
+
+                    });
+               });
+          }
+     }
+}
